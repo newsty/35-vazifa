1da45a60719fbb09e42e87f6dc6ccf573f518ddd
newsty <xadichaermatova19@gmail.com>
2023-07-13 13:08:13 +0500

    call bak

---
+++ main.js
@@ -0,0 +1,184 @@
+
+
+// 1-Vazifa 100 ta call bak function yozish
+
+// function one(a,b,str){
+//  let res=a+b;
+//  return str(res)
+// }
+// function two(num){
+//     return console.log(`yig'indi  ${num} `);
+// }
+// one(7,8,two)
+
+
+
+// function one(num){
+//     return num()
+// }
+// function two(){
+//     console.log('hello');
+// }
+// one(two)
+
+
+// function one(a,b,num){
+//     if(a===b){
+//         num('teng')
+//     }else{
+//         num('teng emas')
+//     }
+// }
+// function two(res){
+//     console.log(res);
+// }
+// one(4,8,two)
+
+
+
+// function one(ntj){
+//     let res=10-8
+//     return ntj(res)
+// }
+// function two(num){
+//     console.log(num);
+// }
+// one(two)
+
+
+// let n=81;
+// function one(res){
+//     let result =n/9;
+// return res(result)
+// }
+// function two(num){
+//     console.log(num);
+// }
+// one(two)
+
+
+// let a='hello', b=7;
+// function one(res){
+//     for(let i=0;i<=b;i++){
+//         console.log(a);
+//     }
+//     return res()
+   
+// }
+
+// function two(num){
+//     console.log(num);
+// }
+// one(two)
+
+
+
+
+ //2-Vazifa setTimautga 50 ta masala yozish
+
+
+
+//  let one=setTimeout(two,1000)
+
+//   function two(){
+//     console.log('hello');
+//   }
+// const stop=clearTimeout(one)
+
+
+// let one=setTimeout(two,2000)
+// function two(){
+//     res=10+12;
+//     return console.log(res);
+// }
+
+// function stop(){
+//     clearTimeout(one)
+// }
+
+
+
+
+// let one=setTimeout(sam,1000)
+
+// let den=8;
+// function sam(){
+//     switch(den){
+//         case 1:
+//            console.log('dushanba');
+//             break;
+//         case 2:
+//            console.log('seshanba');
+//             break; 
+//         case 3:
+//             console.log('chorshanba');
+//             break;
+//         case 4:
+//             console.log('peyshanba');
+//             break;
+//         case 5:
+//             console.log('juma');
+//              break;
+//         case 6:
+//             console.log('shanba');
+//             break; 
+//         case 7:
+//             console.log('yakshanba');
+//             break; 
+//         default:
+//             console.log('bunday kun mavjud emas');     
+//     }
+// }
+
+// let stop=clearTimeout(one)
+
+
+
+
+
+//3-Vazifa setInterval ga 50 ta masala 
+
+
+// let s=0
+
+// function inter(){
+//   s++
+//   console.log(s);
+
+//     if(s===10){
+//         stop()
+//     }
+// }
+// let set=setInterval(inter,1000)
+// function stop(){
+//     clearInterval(set)
+// }
+
+
+
+// function myFunc(){
+//     return console.log('start setInterval'); 
+// }
+// let start=setInterval(myFunc,1000)
+
+// function stop(){
+//     clearInterval(start)
+//     console.log('stop setInterval');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
